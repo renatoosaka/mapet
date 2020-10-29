8ec68a8cd3a46bdef91132fdb534030fb525dfc1
Renato Hideki Osaka <renato.osaka@hotmail.com>
2020-10-29 15:41:20 -0300

    chore: added id to pet view

--- backend/src/views/pet.ts
+++ backend/src/views/pet.ts
@@ -5,17 +5,18 @@ import { lightFormat } from 'date-fns'
 export default {
   render (pet: Pet) {
     return {
+      id: pet.id,
       latitude: pet.latitude,
       longitude: pet.longitude,
       pet_name: pet.pet_name,
-      pet_type: pet.pet_type === 'dog' ? 'Cachorro' : pet.pet_type === 'cat' ? 'Cato' : pet.pet_type,
+      pet_type: pet.pet_type,
       detail: pet.detail,
       when: pet.when ? lightFormat(pet.when, 'dd/MM/yyyy') : null,
       contact_name: pet.contact_name,
       phone_number: pet.phone_number,
-      reward: pet.reward ? Number(pet.reward.toString().replace('.', '').replace(',', '.')).toLocaleString('pt-br', { style: 'currency', currency: 'BRL', minimumFractionDigits: 2 }) : 0.00,
+      reward: pet.reward ? pet.reward : 0.00,
       founded: pet.founded,
-      action_type: pet.action_type === 'L' ? 'Perdi' : 'Encontrei',
+      action_type: pet.action_type,
       images: imagesView.renderMany(pet.images)
     }
   },
